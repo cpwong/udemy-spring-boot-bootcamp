d966f0e9a9ce251790ac4304f2da72c50a0d445d
CP Wong <wongchenpang@gmail.com>
2023-09-28 19:52:57 +0800

    Commit after chapter 145

--- 05. beans/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
+++ 05. beans/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
@@ -13,27 +13,33 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class StoreController {
 
-    StoreService storeService = new StoreService();
+  StoreService storeService;
 
-    @GetMapping("/")
-    public String getForm(Model model, @RequestParam(required = false) String id) {
-        model.addAttribute("item", storeService.getItemfromId(id));
-        return "form";
-    }
+  public StoreController(StoreService storeService) {
+    this.storeService = storeService;
+  }
 
-    @PostMapping("/submitItem")
-    public String handleSubmit(@Valid Item item, BindingResult result, RedirectAttributes redirectAttributes) {
-        if (item.getPrice() < item.getDiscount()) result.rejectValue("price", "", "Price cannot be less than discount");
-        if (result.hasErrors()) return "form";
-        String status = storeService.handleSubmit(item);
-        redirectAttributes.addFlashAttribute("status", status);
-        return "redirect:/inventory";
-    }
+  @GetMapping("/")
+  public String getForm(Model model, @RequestParam(required = false) String id) {
+    model.addAttribute("item", storeService.getItemfromId(id));
+    return "form";
+  }
 
-    @GetMapping("/inventory")
-    public String getInventory(Model model) {
-        model.addAttribute("items", storeService.getItems());
-        return "inventory";
-    }
+  @PostMapping("/submitItem")
+  public String handleSubmit(@Valid Item item, BindingResult result, RedirectAttributes redirectAttributes) {
+    if (item.getPrice() < item.getDiscount())
+      result.rejectValue("price", "", "Price cannot be less than discount");
+    if (result.hasErrors())
+      return "form";
+    String status = storeService.handleSubmit(item);
+    redirectAttributes.addFlashAttribute("status", status);
+    return "redirect:/inventory";
+  }
+
+  @GetMapping("/inventory")
+  public String getInventory(Model model) {
+    model.addAttribute("items", storeService.getItems());
+    return "inventory";
+  }
 
 }
