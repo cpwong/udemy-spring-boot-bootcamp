7ce6bcfa6b2546a224642eee7cb90f6ddef614a3
CP Wong <wongchenpang@gmail.com>
2023-10-10 11:40:03 +0800

    End of day commint

--- 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/repository/ContactRepository.java
+++ 08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/repository/ContactRepository.java
@@ -1,6 +1,8 @@
 package com.ltp.contacts.repository;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
@@ -10,6 +12,12 @@ import com.ltp.contacts.pojo.Contact;
 @Repository
 public class ContactRepository {
     
+    // private List<Contact> contacts = Arrays.asList(
+    //     new Contact("123", "Jon Snow", "1223344"),
+    //     new Contact("456", "Tyrion Lannister", "33445566"),
+    //     new Contact("789", "The Hound", "382978475")
+    // );
+
     private List<Contact> contacts = new ArrayList<>();
 
     public List<Contact> getContacts() {
